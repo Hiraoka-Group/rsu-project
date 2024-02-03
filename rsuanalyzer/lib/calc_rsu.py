a0a7d409858906e5431a4294e0d057d406676473
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-03 20:30:55 +0900

    Make files for chaindrawer package

--- rsuanalyzer/lib/calc_rsu.py
+++ rsuanalyzer/lib/calc_rsu.py
@@ -1,6 +1,16 @@
-def calc_chain_end_dist():
+def calc_chain_end_dist(
+        conf_id: str, theta: float, phi: float, delta_: float
+        ) -> float:
     ...
 
 
-def calc_rsu():
+def calc_chain_length(conf_id: str) -> int:
     ...
+
+
+def calc_rsu(
+        conf_id: str, theta: float, phi: float, delta_: float
+        ) -> float:
+    chain_length = calc_chain_length(conf_id)
+    chain_end_dist = calc_chain_end_dist()
+    return chain_end_dist / chain_length
