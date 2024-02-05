7e4260daacee1c313255cc62c7fb3a1076052960
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-05 09:48:18 +0900

    Refactor calc_chain_end function and fix input validation

--- rsuanalyzer/calc_chain_end.py
+++ rsuanalyzer/calc_chain_end.py
@@ -9,23 +9,21 @@ from .calc_lig_end import calc_lig_end
 def calc_chain_end(
         conf_id: str, theta: float, delta_: float
         ) -> tuple[np.ndarray, R]:
-    conf_id = conf_id.replace("(", "").replace(")", "")
 
     lig_types, con_types = conf_id_to_lig_types_and_con_types(conf_id)
-    x, rot = calc_lig_end(lig_types.popleft(), theta)
+    x, rot = calc_lig_end(lig_types[0], theta)
 
-    while lig_types:
+    for lig_type, con_type in zip(lig_types[1:], con_types):
         x, rot = calc_next_lig_end(
-            lig_types.popleft(), theta, delta_, con_types.popleft(), x, rot)
+            lig_type, theta, delta_, con_type, x, rot)
     return x, rot
 
 
 def calc_next_lig_end(
         lig_type: Literal["RR", "RL", "LR", "LL"],
         theta: float, delta_: float, 
-        con_type: Literal["FF", "FB", "BF", "BB"] | None = None,
-        prev_x: np.ndarray | None = None, 
-        prev_rot: R | None = None,
+        con_type: Literal["FF", "FB", "BF", "BB"],
+        prev_x: np.ndarray, prev_rot: R,
         ) -> tuple[np.ndarray, R]:
     """
     Calculate the position and rotation of the next ligand in the chain.
@@ -36,33 +34,25 @@ def calc_next_lig_end(
         degrees. 0 <= theta <= 90.
     - delta_ (float): Angle in degrees. 0 < delta_ <= 180.
     - con_type (Literal["FF", "FB", "BF", "BB"] | None): Connection type of 
-      the ligand. If the ligand is the first one in the chain, con_type must
-      be None.
-    - prev_x (np.ndarray | None): Position of the previous ligand. If con_type
-      is provided, prev_x must be provided.
-    - prev_rot (R | None): Rotation of the previous ligand. If con_type is
-      provided, prev_rot must be provided.
+      the ligand.
+    - prev_x (np.ndarray): Position of the previous ligand.
+    - prev_rot (R): Rotation of the previous ligand.
 
     Returns:
     - tuple[np.ndarray, R]: Position and rotation of the next ligand.
     """
     # Validate the input.
     if lig_type not in ("RR", "RL", "LR", "LL"):
-        raise ValueError(f"Invalid lig_type: {lig_type}")
+        raise ValueError(
+            f"Invalid lig_type: {lig_type}. "
+            "Expected one of 'RR', 'RL', 'LR', 'LL'.")
     if not 0 <= theta <= 90:
         raise ValueError(f"Invalid theta: {theta}")
     if not 0 < delta_ <= 180:
         raise ValueError(f"Invalid delta_: {delta_}")
-    if con_type is not None and (prev_x is None or prev_rot is None):
-        raise ValueError("x and rot must be provided if con_type is provided.")
-
-    local_dx, local_drot = calc_lig_end(lig_type, theta)
-
-    # If the ligand is the first one in the chain:
-    if con_type is None:
-        return local_dx, local_drot
     
-    # If the ligand is not the first one in the chain:
+    local_dx, local_drot = calc_lig_end(lig_type, theta)
+    
     con_rot = calc_con_rot(con_type, delta_)
     x = prev_x + (prev_rot * con_rot).apply(local_dx)
     rot = prev_rot * con_rot * local_drot
@@ -80,7 +70,6 @@ def conf_id_to_lig_types_and_con_types(
     Returns:
     - tuple[list[str], list[str]]: Ligand types and connection types.
     """
-    conf_id = conf_id.replace("(", "").replace(")", "")
     lig_types = [conf_id[i:i+2] for i in range(0, len(conf_id), 4)]
     con_types = [conf_id[i:i+2] for i in range(2, len(conf_id), 4)]
     return lig_types, con_types
@@ -102,7 +91,7 @@ def calc_con_rot(
     if not 0 < delta_ <= 180:
         raise ValueError(f"Invalid delta_: {delta_}")
     
-    l, m = con_type_to_signs(con_type)
+    l, _ = con_type_to_signs(con_type)
     rot1 = R.from_euler('y', l * delta_, degrees=True)
     if con_type in ("FF", "BB"):
         rot2 = R.from_euler('y', 180, degrees=True)
