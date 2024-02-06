be84cd22499d4342e5b27ff0afafc7ad3165d823
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-06 15:08:29 +0900

    Add min_rsu.py module for calculating minimum RSU values

---
+++ rsuanalyzer/ranker/min_rsu.py
@@ -0,0 +1,34 @@
+from typing import Iterable
+
+import pandas as pd
+
+from ..core.rsu import calc_rsu
+
+
+def calc_min_rsu_for_thetas(
+        ring_ids: Iterable[str], thetas: Iterable[float], delta_: float
+        ) -> pd.DataFrame:
+    
+    min_rsu_list = []
+    for theta in thetas:
+        min_rsu_list.append(calc_min_rsu_for_specific_theta(ring_ids, theta, delta_))
+
+    min_rsu_table = pd.DataFrame({
+        "Theta": thetas,
+        "Ring ID": [min_rsu[0] for min_rsu in min_rsu_list],
+        "RSU": [min_rsu[1] for min_rsu in min_rsu_list]
+    })
+
+    return min_rsu_table
+
+
+def calc_min_rsu_for_specific_theta(
+        ring_ids: Iterable[str], theta: float, delta_: float
+        ) -> tuple[str, float]:
+    
+    rsu_list = [calc_rsu(ring_id, theta, delta_) for ring_id in ring_ids]
+    min_rsu = min(rsu_list)
+    min_rsu_idx = rsu_list.index(min_rsu)
+    min_rsu_ring_id = ring_ids[min_rsu_idx]
+
+    return min_rsu_ring_id, min_rsu
