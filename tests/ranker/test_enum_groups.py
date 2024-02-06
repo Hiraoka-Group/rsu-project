dc950897182cd93a8d8e4ac458282880f6bfbd5c
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-06 17:35:06 +0900

    Fix test_enum_groups

--- tests/ranker/test_enum_groups.py
+++ tests/ranker/test_enum_groups.py
@@ -1,12 +1,15 @@
 from itertools import product
 
 from rsuanalyzer.ranker.conf_group import conf_id_to_group_id
-from rsuanalyzer.ranker.enum_groups import enum_groups
+from rsuanalyzer.ranker.enum_groups import (enum_all_rings,
+                                            enum_groups_from_rings)
 
 
 def test_enum_groups_case1():
-    EXPECTED = {1: {"RRFF", "RRFB", "RRBB", "RLFF", "RLFB", "RLBB"}}
-    assert enum_groups(1) == EXPECTED
+    EXPECTED = {"RRFF", "RRFB", "RRBB", "RLFF", "RLFB", "RLBB"}
+    rings = enum_all_rings(1)
+    groups = enum_groups_from_rings(rings)
+    assert groups == EXPECTED
 
 
 def test_enum_groups_case2():
@@ -15,6 +18,8 @@ def test_enum_groups_case2():
 
     CON_LIG_TYPES = [lig_type + con_type for lig_type, con_type in product(LIG_TYPES, CON_TYPES)]
 
+    rings = enum_all_rings(2)
+    groups = enum_groups_from_rings(rings)
+
     for ring in (id_of_lig1 + id_of_lig2 for id_of_lig1, id_of_lig2 in product(CON_LIG_TYPES, repeat=2)):
-        assert conf_id_to_group_id(ring) in enum_groups(2)[2]
-    
+        assert conf_id_to_group_id(ring) in groups
