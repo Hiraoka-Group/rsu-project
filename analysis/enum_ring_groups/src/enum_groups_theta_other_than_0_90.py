1ceced7ca9242bf14801b3496eecd95b07b295c3
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-10 23:22:32 +0900

    Add scripts to enumerate ring groups for theta of 0 and 90 degrees

--- analysis/enum_ring_groups/src/enum_groups_theta_other_than_0_90.py
+++ analysis/enum_ring_groups/src/enum_groups_theta_other_than_0_90.py
@@ -1,19 +1,20 @@
 import os
 
-from rsuanalyzer.ranker.enum_groups import (enum_all_rings,
-                                            enum_groups_from_rings)
+from rsuanalyzer.ranker.enum_conf_ids import (enum_conf_ids_including_dups,
+                                              exclude_duplicates)
 
+THETA = 30
 OUTPUT_FOLDER = "analysis/enum_ring_groups/output"
 
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
 
 for num_of_ligs in range(1, 5):
-    rings = enum_all_rings(num_of_ligs)
-    groups = enum_groups_from_rings(rings)
+    rings = enum_conf_ids_including_dups(num_of_ligs)
+    groups = exclude_duplicates(rings, THETA)
 
     filename = f"groups (num_ligs={num_of_ligs}).txt"
     
     with open(f"{OUTPUT_FOLDER}/{filename}", "w") as f:
-        f.write("\n".join(groups))
+        f.write("\n".join(sorted(groups)))
     print(f"Saved '{filename}' to {OUTPUT_FOLDER}.")
