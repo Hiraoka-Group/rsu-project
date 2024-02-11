b71f6e76eccb3d8b4e827e9fad6d588f697e7196
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-11 22:39:17 +0900

    Add documentation for ligand.py

--- rsuanalyzer/calc_rsu/ligand.py
+++ rsuanalyzer/calc_rsu/ligand.py
@@ -1,3 +1,9 @@
+"""Functions to calculate vectors and rotations for the ligand.
+
+Model ligand has points A, B, and C, and the coordinate systems A, B1, 
+B2, and C. For more detailed definitions, see the associated paper.
+"""
+
 from typing import Literal
 
 import numpy as np
@@ -7,6 +13,11 @@ from scipy.spatial.transform import Rotation as R
 def x_ac_coord_a(
         lig_type: Literal["RR", "RL", "LR", "LL"], theta: float
         ) -> np.ndarray:
+    """Calculate the vector AC in the coordinate system A.
+    
+    For the definitions of the points and the coordinate systems, 
+    see the associated paper.
+    """
     return x_ab_coord_a(lig_type, theta) + x_bc_coord_a(
         lig_type, theta)
 
@@ -14,12 +25,22 @@ def x_ac_coord_a(
 def x_ab_coord_a(
         lig_type: Literal["RR", "RL", "LR", "LL"], theta: float
         ) -> np.ndarray:
+    """Calculate the vector AB in the coordinate system A.
+
+    For the definitions of the points and the coordinate systems, 
+    see the associated paper.
+    """
     return np.array([1, 0, 0])
 
 
 def x_bc_coord_a(
         lig_type: Literal["RR", "RL", "LR", "LL"], theta: float
         ) -> np.ndarray:
+    """Calculate the vector BC in the coordinate system A.
+
+    For the definitions of the points and the coordinate systems,
+    see the associated paper.
+    """
     if lig_type in ("RR", "RL"):
         return R.from_euler(
             "XZ", [theta, 60], degrees=True).apply([1, 0, 0])
@@ -33,6 +54,11 @@ def x_bc_coord_a(
 def rot_ab1(
         lig_type: Literal["RR", "RL", "LR", "LL"], theta: float
         ) -> R:
+    """Calculate the rotation from the coordinate system A to B1.
+
+    For the definitions of the points and the coordinate systems,
+    see the associated paper.
+    """
     if lig_type in ("RR", "RL"):
         return R.from_euler("X", theta, degrees=True)
     elif lig_type in ("LR", "LL"):
@@ -44,7 +70,15 @@ def rot_ab1(
 def rot_ac(
         lig_type: Literal["RR", "RL", "LR", "LL"], theta: float
         ) -> R:
+    """Calculate the rotation from the coordinate system A to C.
+
+    For the definitions of the points and the coordinate systems,
+    see the associated paper.
+    """
     if lig_type == "RR":
+        # Last rotation of 180 degrees is for ensuring that the
+        # z-axis of the coordinate system C protrudes from the
+        # "Front" side of the pyridine ring.
         return R.from_euler(
             "XZX", [theta, 60, theta + 180], degrees=True)
     elif lig_type == "RL":
@@ -52,6 +86,9 @@ def rot_ac(
     elif lig_type == "LR":
         return R.from_euler("XZX", [-theta, -60, theta], degrees=True)
     elif lig_type == "LL":
+        # Last rotation of 180 degrees is for ensuring that the
+        # z-axis of the coordinate system C protrudes from the
+        # "Front" side of the pyridine ring.
         return R.from_euler(
             "XZX", [-theta, -60, -theta + 180], degrees=True)
     else:
