0b2faaca57d0b7cf7849aec6b95ef6d310071da2
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-11 19:49:38 +0900

    Add docstrings to all functions in enum_including_dups.py and exclude_dups

--- rsuanalyzer/enum_ids/enum_including_dups.py
+++ rsuanalyzer/enum_ids/enum_including_dups.py
@@ -2,29 +2,32 @@ from itertools import product
 
 
 def enum_conf_ids_including_dups(num_of_ligs: int) -> set[str]:
-    """
-    Enumerate all possible conformation IDs of rings.
+    """Enumerate all possible conformation IDs of rings.
 
     Note that this function includes duplicate conformation IDs, i.e.,
-    conformation IDs that represent the same ring structure.
+    conformation IDs that represent the same ring structure, e.g. 
+    "RRFFLLBB" and "LLBBRRFF", or "RRFFRRFF" and "LLFFLLFF".
 
     Args:
-    - num_of_ligs (int): The number of ligands in a ring.
+        num_of_ligs (int): 
+            The number of ligands in a ring.
 
     Returns:
-    - set[str]: The set of conformation IDs of rings.
+        set[str]: 
+            The set of possible conformation IDs of rings with the 
+            given number of ligands.
 
     Examples:
-    >>> enum_conf_ids_including_dups(1)
-    ['RRFF', 'RRFB', 'RRBF', 'RRBB', 'RLFF', ... 'LLBB'] (16 items)
-    >>> enum_conf_ids_including_dups(2)
-    ['RRFFRRFF', ... 'LLBBLLBB'] (256 items)
+        >>> enum_conf_ids_including_dups(1)
+        ['RRFF', 'RRFB', 'RRBF', 'RRBB', 'RLFF', ..., 'LLBB'] (16 items)
+        >>> enum_conf_ids_including_dups(2)
+        ['RRFFRRFF', ... 'LLBBLLBB'] (256 items)
     """
     LIG_TYPES = ["RR", "RL", "LR", "LL"]
     CON_TYPES = ["FF", "FB", "BF", "BB"]
 
     # All combinations of ligand types and connection types.
-    # ["RRFF", "RRFB", "RRBF", "RRBB", "RLFF", ... "LLBB"]
+    # ["RRFF", "RRFB", "RRBF", "RRBB", "RLFF", ... "LLBB"] (16 items)
     LIG_CON_TYPES = [
         lig_type + con_type
         for lig_type, con_type in product(LIG_TYPES, CON_TYPES)]
