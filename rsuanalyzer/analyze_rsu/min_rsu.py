c03e9cf4a924b9323da0640fbe91580c1a9d8bb4
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-02-12 09:32:09 +0900

    Add docstrings in make_rank_table.py and min_rsu.py

--- rsuanalyzer/analyze_rsu/min_rsu.py
+++ rsuanalyzer/analyze_rsu/min_rsu.py
@@ -1,3 +1,5 @@
+"""Functions to calculate the minimum RSU for each theta."""
+
 from typing import Iterable
 
 import pandas as pd
@@ -6,13 +8,27 @@ from ..calc_rsu.rsu import calc_rsu
 
 
 def calc_min_rsu_for_thetas(
-        ring_ids: Iterable[str], thetas: Iterable[float], delta_: float
+        conf_ids: Iterable[str], thetas: Iterable[float], delta_: float
         ) -> pd.DataFrame:
-    
+    """Calculate the minimum RSU for each theta.
+
+    Args:
+        conf_ids (Iterable[str]): 
+            The list of conformation IDs of rings, e.g. "RRFFLLBB".
+        thetas (Iterable[float]): 
+            The list of tilt angles of C-C bonds. (unit: degree) 
+            0 <= theta <= 90.
+        delta_ (float): 
+            N-Pd-N angle. (unit: degree) 0 < delta_ <= 180.
+
+    Returns:
+        A pandas DataFrame containing the minimum RSU for each theta.
+        The columns are "Theta", "Ring ID", and "RSU".
+    """
     min_rsu_list = []
     for theta in thetas:
         min_rsu_list.append(
-            _calc_min_rsu_for_specific_theta(ring_ids, theta, delta_))
+            _calc_min_rsu_for_specific_theta(conf_ids, theta, delta_))
 
     min_rsu_table = pd.DataFrame({
         "Theta": thetas,
@@ -26,6 +42,22 @@ def calc_min_rsu_for_thetas(
 def _calc_min_rsu_for_specific_theta(
         ring_ids: Iterable[str], theta: float, delta_: float
         ) -> tuple[str, float]:
+    """Calculate the minimum RSU for a specific theta.
+    
+    Args:
+        ring_ids (Iterable[str]): 
+            The list of conformation IDs of rings, e.g. "RRFFLLBB".
+        theta (float): 
+            The tilt angle of C-C bonds. (unit: degree) 
+            0 <= theta <= 90.
+        delta_ (float): 
+            N-Pd-N angle. (unit: degree) 0 < delta_ <= 180.
+        
+    Returns:
+        tuple[str, float]: 
+            The conformation ID of the ring with the minimum RSU 
+            and the minimum RSU.
+    """
     
     rsu_list = [
         calc_rsu(ring_id, theta, delta_) for ring_id in ring_ids]
